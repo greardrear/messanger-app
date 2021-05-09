1ed47279aabe3896600a51766b96f85f5faa933c
Marko Stanojevic <82764329+greardrear@users.noreply.github.com>
2021-05-09 13:53:58 +0200

    Add files via upload

---
+++ src/fontawesome.js
@@ -0,0 +1,11 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { faCode, faHighlighter, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faUser,
+  faLock,
+  
+);
